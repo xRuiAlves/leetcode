c70882b902e7fbd00d622f67a45420390539eb47
Rui Alves <up201606746@fe.up.pt>
2019-11-01 13:42:48 +0000

    Add 66 Solution

---
+++ Problems/66. Plus One/Solution.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        if (digits[digits.length -1] != 9) {
+            digits[digits.length - 1] += 1;
+            return digits;
+        }
+             
+        boolean carry = true;
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] == 9) {
+                digits[i] = 0;
+                carry = true;
+            } else {
+                carry = false;
+                digits[i] += 1;
+                break;
+            }
+        }
+        
+        if (carry) {
+            int[] ans = new int[digits.length + 1];
+            ans[0] = 1;
+            for (int i=0 ; i < digits.length; i++) {
+                ans[i+1] = 0;
+            }
+            return ans;
+        }   
+        
+        return digits;
+    }
+}
