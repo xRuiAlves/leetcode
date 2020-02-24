08d7d5c44cde53c148523dc7028c90cf3811d6ac
Rui Alves <up201606746@fe.up.pt>
2020-02-24 21:10:29 +0000

    Improve 40 Solution

--- Problems/40. Combination Sum II/Solution.java
+++ Problems/40. Combination Sum II/Solution.java
@@ -1,27 +1,33 @@
+import java.util.*;
+
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
         List<List<Integer>> res = new LinkedList<>();
+
         visit(candidates, target, res, new LinkedList<>(), 0);
+
         return res;
     }
 
-    public void visit(int[] nums, int sum, List<List<Integer>> res, LinkedList<Integer> curr, int idx) {
-        if (sum < 0) {
+    private void visit(int[] nums, int sum, List<List<Integer>> res, LinkedList<Integer> curr, int idx) {
+        if (sum == 0) {
+            res.add(new LinkedList<>(curr));
             return;
         }
-        if (sum == 0) {
-            LinkedList<Integer> candidate = new LinkedList<>(curr);
-            Collections.sort(candidate);
-            if (!res.contains(candidate)) {
-                res.add(candidate);
-            }
+
+        if (sum < 0) {
             return;
         }
 
         for (int i = idx; i < nums.length; ++i) {
-            curr.addLast(nums[i]);
+            curr.add(nums[i]);
             visit(nums, sum - nums[i], res, curr, i + 1);
             curr.removeLast();
+
+            while (i < (nums.length - 1) && nums[i] == nums[i + 1]) {
+                ++i;
+            }
         }
     }
 }
