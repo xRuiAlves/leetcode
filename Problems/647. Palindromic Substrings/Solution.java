c9f0afc3cc4ddbb808c92946743fae4da990b1e3
Rui Alves <up201606746@fe.up.pt>
2020-02-25 13:40:16 +0000

    Add 647 Solution

---
+++ Problems/647. Palindromic Substrings/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int res = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            ++res;
+            int l = i, r = i;
+            while (r < s.length() - 1 && s.charAt(r) == s.charAt(r + 1)) {
+                ++r;
+                res += (r - i + 1);
+            }
+            int next_index = r;
+
+            while(l > 0 && r < s.length() - 1) {
+                --l;
+                ++r;
+                if (s.charAt(l) != s.charAt(r)) {
+                    break;
+                }
+                ++res;
+            }
+
+            i = next_index;
+        }
+
+        return res;
+    }
+}
