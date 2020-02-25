a7e51360bb9be96e97dbe91b4fba191997d3cfb2
Rui Alves <up201606746@fe.up.pt>
2020-02-25 18:07:06 +0000

    Add 279 Solution

---
+++ Problems/279. Perfect Squares/Solution.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int numSquares(int n) {
+        List<Integer> squares = new LinkedList<>();
+        for (int i = 1; ; ++i) {
+            int square = i*i;
+            if (square <= n) {
+                squares.add(square);
+            } else {
+                break;
+            }
+        }
+
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; ++i) {
+            dp[i] = Integer.MAX_VALUE;
+            for (int j = 1; j * j <= i; ++j) {
+                dp[i] = Math.min(dp[i], dp[i - j*j] + 1);
+            }
+        }
+
+        return dp[n];
+    }
+}
