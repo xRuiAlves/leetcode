693ad1a11fa5ab6118c7c107b2b1451040ebf52a
Rui Alves <up201606746@fe.up.pt>
2020-02-25 19:16:55 +0000

    Add 983 Solution

---
+++ Problems/983. Minimum Cost For Tickets/Solution.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public int mincostTickets(int[] days, int[] costs) {
+        int last_day = days[days.length - 1];
+        int[] dp = new int[last_day + 1];
+        boolean[] traveling = new boolean[last_day + 1];
+
+        for (int travel_day : days) {
+            traveling[travel_day] = true;
+        }
+
+        dp[0] = 0;
+        for (int i = 1; i <= last_day; ++i) {
+            if (!traveling[i]) {
+                dp[i] = dp[i-1];
+            } else {
+                dp[i] = dp[i-1] + costs[0];
+
+                if (i - 7 >= 0) {
+                    dp[i] = Math.min(dp[i], dp[i - 7] + costs[1]);
+                } else {
+                    dp[i] = Math.min(dp[i], costs[1]);
+                }
+
+                if (i - 30 >= 0) {
+                    dp[i] = Math.min(dp[i], dp[i - 30] + costs[2]);
+                } else {
+                    dp[i] = Math.min(dp[i], costs[2]);
+                }
+            }
+        }
+
+        return dp[last_day];
+    }
+}
