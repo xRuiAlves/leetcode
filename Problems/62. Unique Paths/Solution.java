db2da39a69456bef2e6df3d5acf7d1e388197ee9
Rui Alves <up201606746@fe.up.pt>
2020-02-27 15:52:47 +0000

    Add 62 Solution

---
+++ Problems/62. Unique Paths/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[] dp = new int[m];
+        dp[0] = 1;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 1; j < m; ++j) {
+                dp[j] += dp[j - 1];
+            }
+        }
+        return dp[m - 1];
+    }
+}
