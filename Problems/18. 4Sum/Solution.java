f94bae17d44ed5e601f9cbd8f59eb3a3f7743079
Rui Alves <up201606746@fe.up.pt>
2020-02-29 16:07:13 +0000

    Improve 18 Solution

--- Problems/18. 4Sum/Solution.java
+++ Problems/18. 4Sum/Solution.java
@@ -1,31 +1,43 @@
+import java.util.*;
+
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        Arrays.sort(nums);
         List<List<Integer>> res = new LinkedList<>();
+        HashSet<List<Integer>> used = new HashSet<>();
+        Arrays.sort(nums);
 
-        for (int i = 0; i < nums.length - 3; ++i) {
-            for (int j = i + 1; j < nums.length - 2; ++j) {
-                if (j > i + 1 && nums[j] == nums[j - 1]) {
+        for (int i = 0; i < nums.length; ++i) {
+            if (i > 0 && nums[i - 1] == nums[i]) {
+                continue;
+            }
+            for (int j = i + 1; j < nums.length; ++j) {
+                if (nums[i] != nums[j] && nums[j - 1] == nums[j]) {
                     continue;
                 }
+                int l = j + 1;
+                int r = nums.length - 1;
+                int partial_sum = nums[i] + nums[j];
 
-                int left = j + 1;
-                int right = nums.length - 1;
-
-                while (left < right) {
-                    int sum = nums[i] + nums[j] + nums[left] + nums[right];
-
+                while (l < r) {
+                    int sum = partial_sum + nums[l] + nums[r];
                     if (sum == target) {
-                        List<Integer> possibility = new LinkedList<>(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
-                        if (!res.contains(possibility)) {
-                            res.add(possibility);
+                        List<Integer> candidate = Arrays.asList(nums[i], nums[j], nums[l], nums[r]);
+                        if (!used.contains(candidate)) {
+                            used.add(candidate);
+                            res.add(candidate);
                         }
-                    }
-
-                    if (sum < target) {
-                        ++left;
+                        ++l;
+                        --r;
+                        while (l < r && nums[l] == nums[l - 1]) {
+                            ++l;
+                        }
+                        while (l < r && r + 1 < nums.length && nums[r] == nums[r + 1]) {
+                            --r;
+                        }
+                    } else if (sum > target) {
+                        --r;
                     } else {
-                        --right;
+                        ++l;
                     }
                 }
             }
