1b9fb8a8aec703880a4b77e906d6be8c08a9c51b
Rui Alves <up201606746@fe.up.pt>
2020-02-29 12:39:26 +0000

    Add 61 Solution

---
+++ Problems/61. Rotate List/Solution.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        ListNode node = head;
+        ListNode tail = node;
+        int size = 0;
+        while (node != null) {
+            ++size;
+            tail = node;
+            node = node.next;
+        }
+
+        if (size == 0 || size == 1) {
+            return head;
+        }
+
+        int rotation_amount = k % size;
+        if (rotation_amount == 0) {
+            return head;
+        }
+
+        tail.next = head;
+        node = head;
+        for (int i = 0; i < size - rotation_amount - 1; ++i) {
+            node = node.next;
+        }
+        
+        ListNode ret = node.next;
+        node.next = null;
+        return ret;
+    }
+}
