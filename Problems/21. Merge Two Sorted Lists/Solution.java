49ebf6b2bbfbcf2628ffb76b8d6705cc3ff24154
Rui Alves <up201606746@fe.up.pt>
2020-03-15 16:28:59 +0000

    Improve 21 solution

--- Problems/21. Merge Two Sorted Lists/Solution.java
+++ Problems/21. Merge Two Sorted Lists/Solution.java
@@ -8,51 +8,26 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1 == null && l2 == null) {
-            return null;
-        }
-        
-        boolean b1 = false;
-        
-        if (l1 == null) {
-            b1 = false;
-        } else if (l2 == null) {
-            b1 = true;
-        } else {
-            if (l1.val < l2.val) {
-                b1 = true;
-            }
-        }
-        
-        ListNode node = new ListNode(b1 ? l1.val : l2.val);
-        ListNode res = node;
-        if (b1) {
-            l1 = l1.next;
-        } else {
-            l2 = l2.next;
-        }
-        
-        while (l1 != null || l2 != null) {
-            b1 = false;
-            if (l1 != null && l2 != null) {
-                if (l1.val < l2.val) {
-                    b1 = true;
-                }
+        ListNode stub = new ListNode(-1);
+        ListNode curr = stub;
+
+        while (!(l1 == null && l2 == null)) {
+            if (l2 == null) {
+                curr.next = l1;
+                l1 = l1.next;
             } else if (l1 == null) {
-                b1 = false;
-            } else if (l2 == null) {
-                b1 = true;
-            }
-            
-            node.next = new ListNode(b1 ? l1.val : l2.val);
-            node = node.next;
-            if (b1) {
+                curr.next = l2;
+                l2 = l2.next;
+            } else if (l1.val < l2.val) {
+                curr.next = l1;
                 l1 = l1.next;
             } else {
+                curr.next = l2;
                 l2 = l2.next;
-            }            
+            }
+            curr = curr.next;
         }
         
-        return res;
+        return stub.next;
     }
 }
