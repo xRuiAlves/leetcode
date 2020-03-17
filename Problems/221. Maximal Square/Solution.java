5ef2a1d72374472cfe60202521f5b9cd16bb6038
Rui Alves <up201606746@fe.up.pt>
2020-03-17 20:57:17 +0000

    Improve 221 solution

--- Problems/221. Maximal Square/Solution.java
+++ Problems/221. Maximal Square/Solution.java
@@ -1,35 +1,36 @@
 class Solution {
     public int maximalSquare(char[][] matrix) {
-        int max = 0;
-        for (int i = 0; i < matrix.length; ++i) {
-            for (int j = 0; j < matrix[i].length; ++j) {
-                max = Math.max(max, getSquareSize(matrix, i, j));
-            }
-        }
-        return max*max;
-    }
-
-    private int getSquareSize(char[][] matrix, int i, int j) {
-        if (matrix[i][j] != '1') {
+        if (matrix.length == 0 || matrix[0].length == 0) {
             return 0;
         }
+        
+        int[][] dp = new int[matrix.length][matrix[0].length];
+        int max = 0;
 
-        int layer = 1;
-        boolean is_valid = true;
-        while (is_valid && (i + layer - 1 < matrix.length) && (j + layer - 1 < matrix[i].length)) {
-            ++layer;
-            for (int a = 0; a < layer && i + a < matrix.length; ++a) {
-                for (int b = 0; b < layer && j + b < matrix[i].length; ++b) {
-                    if (a == layer - 1 || b == layer - 1) {
-                        if (matrix[i + a][j + b] != '1') {
-                            is_valid = false;
-                            break;
-                        }
-                    }
+        for (int i = 0; i < matrix.length; ++i) {
+            dp[i][0] = matrix[i][0] == '1' ? 1 : 0;
+            if (matrix[i][0] == '1') {
+                max = 1;
+            }
+        }
+        for (int j = 0; j < matrix[0].length; ++j) {
+            dp[0][j] = matrix[0][j] == '1' ? 1 : 0;
+            if (matrix[0][j] == '1') {
+                max = 1;
+            }
+        }
+
+        for (int i = 1; i < matrix.length; ++i) {
+            for (int j = 1; j < matrix[i].length; ++j) {
+                if (matrix[i][j] == '1') {
+                    dp[i][j] = 1 + Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]);
+                    max = Math.max(max, dp[i][j]);
+                } else {
+                    dp[i][j] = 0;
                 }
             }
         }
 
-        return layer - 1;
+        return max * max;
     }
 }
