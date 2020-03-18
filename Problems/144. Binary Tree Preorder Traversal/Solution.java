42c7c253cfd3975766d7a389d053150deaa27ed9
Rui Alves <up201606746@fe.up.pt>
2020-03-18 14:13:59 +0000

    Add 144 solution

---
+++ Problems/144. Binary Tree Preorder Traversal/Solution.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new LinkedList<>();
+        if (root == null) {
+            return res;
+        }
+        
+        Stack<TreeNode> stack = new Stack<>();
+        stack.add(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode curr = stack.pop();
+            res.add(curr.val);
+            if (curr.right != null) {
+                stack.add(curr.right);
+            }
+            if (curr.left != null) {
+                stack.add(curr.left);
+            }
+        }
+
+        return res;
+    }
+}
