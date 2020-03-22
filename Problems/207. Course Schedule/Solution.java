2fe8fb4a95f259888eb321558b391891c2c673e3
Rui Alves <up201606746@fe.up.pt>
2020-03-22 11:26:46 +0000

    Improve 207 solution

--- Problems/207. Course Schedule/Solution.java
+++ Problems/207. Course Schedule/Solution.java
@@ -1,54 +1,47 @@
+import java.util.*;
+
 class Solution {
-    public boolean graphHasCycles(HashMap<Integer, HashSet<Integer>> m, int numCourses) {
-        boolean[] visited = new boolean[numCourses];
-        boolean[] depth_stack = new boolean[numCourses];
-
-        for (int i = 0; i < m.size(); ++i) {
-            if (graphHasCycles(m, visited, depth_stack, i)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    public boolean graphHasCycles(HashMap<Integer, HashSet<Integer>> m, boolean[] visited, boolean[] depth_stack, int vertex) {
-        if (!m.containsKey(vertex)) {
-            return false;
-        }
-
-        if (depth_stack[vertex]) {
-            return true;
-        }
-
-        if (visited[vertex]) {
-            return false;
-        }
-
-        visited[vertex] = true;
-        depth_stack[vertex] = true;
-
-        for (int neighbor : m.get(vertex)) {
-            if (graphHasCycles(m, visited, depth_stack, neighbor)) {
-                return true;
-            }
-        }
-
-        depth_stack[vertex] = false;
-        return false;
-    }
-
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        HashMap<Integer, HashSet<Integer>> m = new HashMap<>();
+        ArrayList<HashSet<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < numCourses; ++i) {
+            graph.add(new HashSet<>());
+        }
 
         for (int i = 0; i < prerequisites.length; ++i) {
             int from = prerequisites[i][1];
             int to = prerequisites[i][0];
-
-            m.putIfAbsent(from, new HashSet<>());
-            m.get(from).add(to);
+            graph.get(from).add(to);
         }
 
-        return !graphHasCycles(m, numCourses);
+        boolean[] depth_stack = new boolean[numCourses];
+        boolean[] visited = new boolean[numCourses];
+
+        for (int i = 0; i < numCourses; ++i) {
+            if (hasCycles(graph, depth_stack, visited, i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean hasCycles(ArrayList<HashSet<Integer>> graph, boolean[] depth_stack, boolean[] visited, int node) {
+        if (depth_stack[node]) {
+            return true;
+        }
+        if (visited[node]) {
+            return false;
+        }
+
+        depth_stack[node] = true;
+        visited[node] = true;
+
+        for (int neighbor : graph.get(node)) {
+            if (hasCycles(graph, depth_stack, visited, neighbor)) {
+                return true;
+            }
+        }
+
+        depth_stack[node] = false;
+        return false;
     }
 }
