9b6fd035af1ba886651101987dcb99dd47150b77
Rui Alves <up201606746@fe.up.pt>
2020-04-02 11:21:34 +0100

    Add april 2020 challenge day 1 solution

---
+++ April 2020 Monthly Challenge/Day1.java
@@ -0,0 +1,16 @@
+public class Day1 {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer, Integer> counts = new HashMap<>();
+        for (int num : nums) {
+            counts.put(num, counts.getOrDefault(num, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> num_count : counts.entrySet()) {
+            if (num_count.getValue() == 1) {
+                return num_count.getKey(); 
+            }
+        }
+        
+        return -1;
+    }
+}
