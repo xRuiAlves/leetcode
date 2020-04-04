d9d13b0626c7e4a9ab349c8ad4d8d000272e2413
Rui Alves <up201606746@fe.up.pt>
2020-04-04 12:33:36 +0100

    Add 202 solution

---
+++ Problems/202. Happy Number/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> visited = new HashSet<>();
+        while(n != 1) {
+            if (visited.contains(n)) {
+                return false;
+            }
+            visited.add(n);
+            n = transform(n);
+        }
+        return true;
+    }
+    
+    private int transform(int n) {
+        int res = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            res += digit * digit;
+            n /= 10;
+        }
+        return res;
+    }
+}
