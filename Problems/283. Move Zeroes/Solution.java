3a42605623c71df05b36b41e60912452f31f9a18
Rui Alves <up201606746@fe.up.pt>
2020-04-04 12:29:25 +0100

    Add 283 solution

---
+++ Problems/283. Move Zeroes/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int zero_count = 0;
+
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] == 0) {
+                ++zero_count;
+            } else {
+                int temp = nums[i];
+                nums[i] = nums[i - zero_count];
+                nums[i - zero_count] = temp;
+            }
+        }
+    }
+}
