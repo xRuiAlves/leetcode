97e11727fd4dcbe076412262717dbb2af61ec7f0
Rui Alves <up201606746@fe.up.pt>
2020-04-05 10:25:54 +0100

    Add april 2020 challenge day 5 solution

---
+++ April 2020 Monthly Challenge/Day5.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class Day5 {
+    public int maxProfit(int[] prices) {
+        int[] cache = new int[prices.length];
+        Arrays.fill(cache, -1);
+        return maxProfit(prices, 0, cache);
+    }
+
+    public int maxProfit(int[] prices, int start, int[] cache) {
+        if (start >= prices.length - 1) {
+            return 0;
+        }
+        
+        if (cache[start] != -1) {
+            return cache[start];
+        }
+
+        int best = 0;
+        int min_idx = start;
+
+        for (int i = start + 1; i < prices.length; ++i) {
+            if (prices[i] > prices[min_idx]) {
+                int price_candidate = prices[i] - prices[min_idx] + maxProfit(prices, i + 1, cache);
+                best = Math.max(best, price_candidate);
+            } else if (prices[i] < prices[min_idx]) {
+                min_idx = i;
+            }
+        }
+
+        cache[start] = best;
+        return best;
+    }
+}
