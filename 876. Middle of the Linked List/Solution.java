3cec79bfeec483c19a5185d5028d957a96f51d53
Rui Alves <up201606746@fe.up.pt>
2020-04-08 08:18:19 +0100

    Add 876 solution

---
+++ 876. Middle of the Linked List/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow.next;
+    }
+}
