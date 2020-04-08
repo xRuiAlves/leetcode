813b8ce10ff907f4f673aabd8b0db4ea14fa76c9
Rui Alves <up201606746@fe.up.pt>
2020-04-08 08:17:48 +0100

    Add april 2020 challenge day 8 solution

---
+++ April 2020 Monthly Challenge/Day8.java
@@ -0,0 +1,17 @@
+public class Day8 {
+    public ListNode middleNode(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow.next;
+    }
+}
