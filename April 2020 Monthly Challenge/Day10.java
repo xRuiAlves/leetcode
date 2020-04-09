14e7a3e7fed53a81361ca3bba87902a62d4d6483
Rui Alves <up201606746@fe.up.pt>
2020-04-09 13:45:26 +0100

    Add april 2020 challenge day 10 solution

---
+++ April 2020 Monthly Challenge/Day10.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Day10 {
+    private static final class Pair {
+        public int val;
+        public int min;
+
+        public Pair(int val, int min) {
+            this.val = val;
+            this.min = min;
+        }
+    }
+
+    Stack<Pair> stack = new Stack<>();
+
+    public void push(int x) {
+        int current_min = stack.isEmpty() ? x : stack.peek().min;
+        stack.push(new Pair(x, Math.min(x, current_min)));
+    }
+
+    public void pop() {
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek().val;
+    }
+
+    public int getMin() {
+        return stack.peek().min;
+    }
+}
