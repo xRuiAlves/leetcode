9be04da85320005d8bc7278c4557fb73a1fa2dbb
Rui Alves <up201606746@fe.up.pt>
2020-04-23 09:23:12 +0100

    Add 201 Solution

---
+++ 201. Bitwise AND of Numbers Range/Solution.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int rangeBitwiseAnd(int m, int n) {
+        if (m == n) return m;
+        int num_bits = 1 + (int) (Math.log(n - m)/Math.log(2));
+        int mask = ~((1 << num_bits) - 1);
+        return mask & n & m;
+    }
+}
