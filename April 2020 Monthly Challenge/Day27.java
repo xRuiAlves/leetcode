5ff9d797bbff2fb21840e8c41977e245eb83eb87
Rui Alves <up201606746@fe.up.pt>
2020-04-27 08:52:24 +0100

    Add april 2020 challenge day 27 solution

---
+++ April 2020 Monthly Challenge/Day27.java
@@ -0,0 +1,36 @@
+public class Day27 {
+    public int maximalSquare(char[][] matrix) {
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+        
+        int[][] dp = new int[matrix.length][matrix[0].length];
+        int max = 0;
+
+        for (int i = 0; i < matrix.length; ++i) {
+            dp[i][0] = matrix[i][0] == '1' ? 1 : 0;
+            if (matrix[i][0] == '1') {
+                max = 1;
+            }
+        }
+        for (int j = 0; j < matrix[0].length; ++j) {
+            dp[0][j] = matrix[0][j] == '1' ? 1 : 0;
+            if (matrix[0][j] == '1') {
+                max = 1;
+            }
+        }
+
+        for (int i = 1; i < matrix.length; ++i) {
+            for (int j = 1; j < matrix[i].length; ++j) {
+                if (matrix[i][j] == '1') {
+                    dp[i][j] = 1 + Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]);
+                    max = Math.max(max, dp[i][j]);
+                } else {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
+        return max * max;
+    }
+}
