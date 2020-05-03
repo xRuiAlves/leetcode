6b1c9f8ff2b80283fd860ccc26b130af81f09c9d
Rui Alves <up201606746@fe.up.pt>
2020-05-03 10:01:45 +0100

    Add 383 Solution

---
+++ Problems/383. Ransom Note/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] char_counts = new int[26];
+
+        for (int i = 0; i < magazine.length(); ++i) {
+            ++char_counts[magazine.charAt(i) - 'a'];
+        }
+
+        for (int i = 0; i < ransomNote.length(); ++i) {
+            int val = ransomNote.charAt(i) - 'a';
+            
+            if (char_counts[val] <= 0) {
+                return false;
+            }
+
+            --char_counts[val];
+        }
+
+        return true;
+    }
+}
