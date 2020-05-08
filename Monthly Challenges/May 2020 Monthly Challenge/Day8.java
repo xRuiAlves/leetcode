a2e6cced4bb5bb38ae858fe2c16cea8f477dbf7f
Rui Alves <up201606746@fe.up.pt>
2020-05-08 09:13:15 +0100

    Add may 2020 challenge day 8 solution

---
+++ Monthly Challenges/May 2020 Monthly Challenge/Day8.java
@@ -0,0 +1,24 @@
+public class Day8 {
+    private static final int MAX_SLOPE = Integer.MAX_VALUE;
+
+    public boolean checkStraightLine(int[][] coordinates) {
+        double slope = getSlope(coordinates[0], coordinates[1]);
+
+        for (int i = 1; i < coordinates.length - 1; ++i) {
+            if (getSlope(coordinates[i], coordinates[i + 1]) != slope) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static final double getSlope(int[] p1, int[] p2) {
+        int[] vector = new int[]{
+                p2[0] - p1[0],
+                p2[1] - p1[1]
+        };
+
+        return vector[0] == 0 ? MAX_SLOPE : ((double)vector[1]/vector[0]);
+    }
+}
