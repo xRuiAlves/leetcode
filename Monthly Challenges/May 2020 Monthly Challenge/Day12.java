a93048aaa61ec25a9365d543cad8d352a49bf2c8
Rui Alves <up201606746@fe.up.pt>
2020-05-12 09:17:28 +0100

    Add may 2020 challenge day 12 solution

---
+++ Monthly Challenges/May 2020 Monthly Challenge/Day12.java
@@ -0,0 +1,30 @@
+public class Day12 {
+    public int singleNonDuplicate(int[] nums) {
+        int l = 0;
+        int r = nums.length - 1;
+        boolean mult4;
+
+        while (l < r) {
+            mult4 = ((r - l) % 4) == 0;
+            int m = (l + r)/2;
+
+            if (m + 1 < nums.length && nums[m] == nums[m + 1]) {
+                if (mult4) {
+                    l = m + 2;
+                } else {
+                    r = m - 1;
+                }
+            } else if (m > 0 && nums[m] == nums[m - 1]) {
+                if (mult4) {
+                    r = m - 2;
+                } else {
+                    l = m + 1;
+                }
+            } else {
+                return nums[m];
+            }
+        }
+
+        return nums[l];
+    }
+}
