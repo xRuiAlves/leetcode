948571aaa87b74c02dcd7af32680bcfe15cd84c5
Rui Alves <up201606746@fe.up.pt>
2020-05-18 14:11:03 +0100

    Add 567 solution

---
+++ Problems/567. Permutation in String/Solution.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if (s2.length() < s1.length()) {
+            return false;
+        }
+
+        int len = s1.length();
+
+        int[] s_counts = new int[26];
+        int[] p_counts = new int[26];
+
+        for (int i = 0; i < len; ++i) {
+            ++p_counts[s1.charAt(i) - 'a'];
+            ++s_counts[s2.charAt(i) - 'a'];
+        }
+        if (equalArrays(s_counts, p_counts)) {
+            return true;
+        }
+
+        for (int i = len; i < s2.length(); ++i) {
+            --s_counts[s2.charAt(i - len) - 'a'];
+            ++s_counts[s2.charAt(i) - 'a'];
+            if (equalArrays(s_counts, p_counts)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean equalArrays(int[] a, int[] b) {
+        for (int i = 0; i < a.length; ++i) {
+            if (a[i] != b[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
