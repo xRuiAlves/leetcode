27bfab56ad76ea8224f29022e12395314384637e
Rui Alves <up201606746@fe.up.pt>
2020-05-19 11:15:17 +0100

    Add 901 solution

---
+++ Problems/901. Online Stock Span/Solution.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class StockSpanner {
+    private Stack<Integer> nums, totals;
+
+    public StockSpanner() {
+        nums = new Stack<>();
+        totals = new Stack<>();
+    }
+
+    public int next(int price) {
+        int total = 1;
+        
+        while(!nums.isEmpty() && nums.peek() <= price) {
+            nums.pop();
+            total += totals.pop();
+        }
+        
+        totals.push(total);
+        nums.push(price);
+        return total;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
+ 
