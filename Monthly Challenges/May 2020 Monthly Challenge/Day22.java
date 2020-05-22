cba025f1535c46fbedb8d6081e018e6f585bc0d2
Rui Alves <up201606746@fe.up.pt>
2020-05-22 14:17:12 +0100

    Add may 2020 challenge day 22 solution

---
+++ Monthly Challenges/May 2020 Monthly Challenge/Day22.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+
+public class Day22 {
+    private static final int NUM_CHARS = 256;
+
+    private static final class CharCount {
+        public char character;
+        public int count;
+
+        public CharCount(char character) {
+            this.character = character;
+            this.count = 0;
+        }
+    }
+
+    public String frequencySort(String s) {
+        CharCount[] char_counts = new CharCount[256];
+
+        for (int i = 0; i < NUM_CHARS; ++i) {
+            char_counts[i] = new CharCount((char) i);
+        }
+
+        for (char c : s.toCharArray()) {
+            ++char_counts[c].count;
+        }
+
+        Arrays.sort(char_counts, (cc1, cc2) -> cc2.count - cc1.count);
+
+        StringBuilder sb = new StringBuilder();
+
+        for (CharCount cc : char_counts) {
+            for (int i = 0; i < cc.count; ++i) {
+                sb.append(cc.character);
+            }
+        }
+
+        return sb.toString();
+    }
+}
