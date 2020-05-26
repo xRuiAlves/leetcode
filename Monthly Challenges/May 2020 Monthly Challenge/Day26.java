ae8abf1f76e4de33f16e995a339346ca5eb1e8d7
Rui Alves <up201606746@fe.up.pt>
2020-05-26 09:39:05 +0100

    Add may 2020 challenge day 26 solution

---
+++ Monthly Challenges/May 2020 Monthly Challenge/Day26.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Day26 {
+    private static final int UNVISITED = -2;
+
+    public int findMaxLength(int[] nums) {
+        Map<Integer, Integer> sums = new HashMap<>();
+        int best = 0;
+        int curr = 0;
+        sums.put(0, -1);
+
+        for (int i = 0; i < nums.length; ++i) {
+            curr += nums[i] == 1 ? 1 : -1;
+            
+            int prev_occurence = sums.getOrDefault(curr, UNVISITED);
+            if (prev_occurence != UNVISITED) {
+                best = Math.max(best, i - prev_occurence);
+            } else {
+                sums.put(curr, i);
+            }
+        }
+
+        return best;
+    }
+}
