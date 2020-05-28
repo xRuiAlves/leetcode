de71e8c760bc4b4af987e4c0dcdc7bd5111ccb0a
Rui Alves <up201606746@fe.up.pt>
2020-05-28 12:12:15 +0100

    Add 338 Solution

---
+++ Problems/338. Counting Bits/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    private static final int NUM_BITS = 32;
+
+    public int[] countBits(int num) {
+        int[] res = new int[num + 1];
+
+        for (int n = 1; n <= num; ++n) {
+            for (int b = 0; b < NUM_BITS; ++b) {
+                if ((n & (1 << b)) != 0) {
+                    ++res[n];
+                }
+            }
+        }
+
+        return res;
+    }
+}
