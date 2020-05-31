4cff2c5901e4ea11a06ca812402b8111df65f0b2
Rui Alves <up201606746@fe.up.pt>
2020-05-31 09:37:42 +0100

    Add 72 Solution

---
+++ Problems/72. Edit Distance/Solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+        int[][] dp = new int[n + 1][m + 1];
+
+        for (int i = 0; i <= n; ++i) {
+            dp[i][0] = i;
+        }
+
+        for (int j = 0; j <= m; ++j) {
+            dp[0][j] = j;
+        }
+
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= m; ++j) {
+                dp[i][j] = Math.min(
+                    1 + Math.min(dp[i - 1][j], dp[i][j - 1]),
+                    dp[i - 1][j - 1] + (word1.charAt(i - 1) == word2.charAt(j - 1) ? 0 : 1)
+                );
+            }
+        }
+
+        return dp[n][m];
+    }
+}
