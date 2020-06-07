a7354286c94df65121cd5800bc8fae97ce7ae28d
Rui Alves <up201606746@fe.up.pt>
2020-06-07 09:29:53 +0100

    Add june 2020 challenge day 7 solution

---
+++ Monthly Challenges/June 2020 Monthly Challenge/Day7.java
@@ -0,0 +1,14 @@
+public class Day7 {
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; ++i) {
+                dp[i] += dp[i - coin];
+            }
+        }
+
+        return dp[amount];
+    }
+}
