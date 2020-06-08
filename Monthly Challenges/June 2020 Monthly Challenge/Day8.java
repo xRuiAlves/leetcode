b63d39c71be1bc8c690d0dc9ecd0ee4d77d3b6e2
Rui Alves <up201606746@fe.up.pt>
2020-06-08 09:34:42 +0100

    Add june 2020 challenge day 8 solution

---
+++ Monthly Challenges/June 2020 Monthly Challenge/Day8.java
@@ -0,0 +1,20 @@
+public class Day8 {
+    public boolean isPowerOfTwo(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        
+        boolean bit_found = false;
+
+        for (int i = 0; i < 32; ++i) {
+            if ((n & (1 << i)) != 0) {
+                if (bit_found) {
+                    return false;
+                }
+                bit_found = true;
+            }
+        }
+
+        return bit_found;
+    }
+}
