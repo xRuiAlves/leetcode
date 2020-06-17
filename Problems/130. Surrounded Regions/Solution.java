aef4755d142095c405b977122f4d82349daadaa1
Rui Alves <up201606746@fe.up.pt>
2020-06-17 09:57:11 +0100

    Improve 130 solution

--- Problems/130. Surrounded Regions/Solution.java
+++ Problems/130. Surrounded Regions/Solution.java
@@ -1,14 +1,18 @@
 class Solution {
-    private boolean border_touched = false;
+    private static final char CAPTURED = 'X';
+    private static final char TARGET = 'O';
+    private static final char OUT_OF_BOUNDS = ' ';
+    private static final char ANALYSING = '?';
+    private static final char BAD_CANDIDATE = '.';
 
     public void solve(char[][] board) {
         for (int i = 0; i < board.length; ++i) {
             for (int j = 0; j < board[i].length; ++j) {
-                if (board[i][j] == 'O') {
-                    border_touched = false;
-                    flood(board, i, j, 'O', 'P');
-                    if (!border_touched) {
-                        flood(board, i, j, 'P', 'X');
+                if (board[i][j] == TARGET) {
+                    if (shouldCapture(board, i, j)) {
+                        markSpot(board, i, j);
+                    } else {
+                        board[i][j] = BAD_CANDIDATE;
                     }
                 }
             }
@@ -16,30 +20,72 @@ class Solution {
 
         for (int i = 0; i < board.length; ++i) {
             for (int j = 0; j < board[i].length; ++j) {
-                if (board[i][j] == 'P') {
-                    board[i][j] = 'O';
+                if (board[i][j] != TARGET && board[i][j] != CAPTURED) {
+                    board[i][j] = TARGET;
                 }
             }
         }
     }
 
-    private void flood(char[][] board, int i, int j, char old, char change) {
-        if (i < 0 || i >= board.length || j < 0 || j >= board[i].length || board[i][j] != old || board[i][j] == change) {
+    private boolean shouldCapture(char[][] board, int i, int j) {
+        char u = visit(board, i - 1, j);
+        char d = visit(board, i + 1, j);
+        char l = visit(board, i, j - 1);
+        char r = visit(board, i, j + 1);
+
+        if (isBadCell(u) || isBadCell(d) || isBadCell(l) || isBadCell(r)) {
+            board[i][j] = BAD_CANDIDATE;
+            return false;
+        }
+
+        board[i][j] = ANALYSING;
+
+        if (u == TARGET && !shouldCapture(board, i - 1, j)) {
+            board[i][j] = BAD_CANDIDATE;
+            return false;
+        }
+        if (d == TARGET && !shouldCapture(board, i + 1, j)) {
+            board[i][j] = BAD_CANDIDATE;
+            return false;
+        }
+        if (l == TARGET && !shouldCapture(board, i, j - 1)) {
+            board[i][j] = BAD_CANDIDATE;
+            return false;
+        }
+        if (r == TARGET && !shouldCapture(board, i, j + 1)) {
+            board[i][j] = BAD_CANDIDATE;
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean isBadCell(char cell) {
+        return (cell == BAD_CANDIDATE || cell == OUT_OF_BOUNDS);
+    }
+
+    private boolean isMarkableCell(char cell) {
+        return (cell == TARGET || cell == ANALYSING);
+    }
+
+    private char visit(char[][] board, int i, int j) {
+        return isOutOfBounds(board, i, j) ? OUT_OF_BOUNDS : board[i][j];
+    }
+
+    private boolean isOutOfBounds(char[][] board, int i, int j) {
+        return (i < 0 || i >= board.length || j < 0 || j >= board[i].length);
+    }
+
+    private void markSpot(char[][] board, int i, int j) {
+        if (isOutOfBounds(board, i, j) || !isMarkableCell(board[i][j])) {
             return;
         }
 
-        if (isBorder(board, i, j)) {
-            border_touched = true;
-        }
+        board[i][j] = CAPTURED;
 
-        board[i][j] = change;
-        flood(board, i + 1, j, old, change);
-        flood(board, i - 1, j, old, change);
-        flood(board, i, j + 1, old, change);
-        flood(board, i, j - 1, old, change);
-    }
-
-    private boolean isBorder(char[][] board, int i, int j) {
-        return i == 0 || j == 0 || i == board.length - 1 || j == board[i].length - 1;
+        markSpot(board, i - 1, j);
+        markSpot(board, i + 1, j);
+        markSpot(board, i, j - 1);
+        markSpot(board, i, j + 1);
     }
 }
