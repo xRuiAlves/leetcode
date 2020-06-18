5b9aab35ae9b8700a9df9e782ea83b6fc639f2ac
Rui Alves <up201606746@fe.up.pt>
2020-06-18 09:54:03 +0100

    Improve 275 solution

---
+++ Problems/275. H-Index II/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        int l = 0;
+        int r = n - 1;
+
+        while(l <= r) {
+            int m = (l + r)/2;
+
+            if(citations[m] >= n - m) {
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+
+        return n - l;
+    }
+}
