9e10da00b6618f87268500ea770cb338024ed120
Rui Alves <up201606746@fe.up.pt>
2020-06-23 09:29:40 +0100

    Add june 2020 challenge day 23 solution

---
+++ Monthly Challenges/June 2020 Monthly Challenge/Day23.java
@@ -0,0 +1,40 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Day23 {
+    public int countNodes(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int count = 0;
+
+        while (!queue.isEmpty()) {
+            TreeNode curr = queue.poll();
+
+            if (curr == null) {
+                return count;
+            }
+
+            ++count;
+            queue.offer(curr.left);
+            queue.offer(curr.right);
+        }
+
+        return count;
+    }
+}
