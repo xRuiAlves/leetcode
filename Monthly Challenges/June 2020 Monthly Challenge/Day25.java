548bf183cf4ae2aaccba9c12b992a9d3918a39d2
Rui Alves <up201606746@fe.up.pt>
2020-06-25 20:55:45 +0100

    Add june 2020 challenge day 25 solution

---
+++ Monthly Challenges/June 2020 Monthly Challenge/Day25.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Day25 {
+    public int findDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; ++i) {
+            if (nums[i] == nums[i + 1]) {
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}
