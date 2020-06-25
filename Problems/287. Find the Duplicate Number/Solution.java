5feed724062f3869d0ce917919cd4ba9c69221b0
Rui Alves <up201606746@fe.up.pt>
2020-06-25 20:56:41 +0100

    Improve 287 solution

---
+++ Problems/287. Find the Duplicate Number/Solution.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; ++i) {
+            if (nums[i] == nums[i + 1]) {
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}
