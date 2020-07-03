c1befbd1f66a8b4d58c9f8731e0cb63ea417e4fc
Rui Alves <up201606746@fe.up.pt>
2020-07-03 23:16:44 +0100

    Add 957 solution

---
+++ Problems/957. Prison Cells After N Days/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    private static final int LOOP_SIZE = 14;
+
+    public int[] prisonAfterNDays(int[] cells, int N) {
+        int num_iters = N % LOOP_SIZE != 0 ? N % LOOP_SIZE : LOOP_SIZE;
+
+        for (int i = 0; i < num_iters; ++i) {
+            int last = cells[0];
+            cells[0] = 0;
+
+            for (int j = 1; j < 7; ++j) {
+                int curr = cells[j];
+                cells[j] = (last == cells[j + 1]) ? 1 : 0;
+                last = curr;
+            }
+
+            cells[7] = 0;
+        }
+        
+        return cells;
+    }
+}
