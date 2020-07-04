670d9f62afd87711b4e4c4ce19edf088ce9ef8a5
Rui Alves <up201606746@fe.up.pt>
2020-07-04 09:13:11 +0100

    Add july 2020 challenge day 4 solution

---
+++ Monthly Challenges/July 2020 Monthly Challenge/Day4.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Day4 {
+    public int nthUglyNumber(int n) {
+        int count = 0;
+        TreeSet<Integer> uglies = new TreeSet<>();
+        uglies.add(1);
+
+        while(true) {
+            int num = uglies.pollFirst();
+            ++count;
+
+            if (count == n) {
+                return num;
+            }
+
+            if (num <= Integer.MAX_VALUE / 2) {
+                uglies.add(num * 2);
+            }
+            if (num <= Integer.MAX_VALUE / 3) {
+                uglies.add(num * 3);
+            }
+            if (num <= Integer.MAX_VALUE / 5) {
+                uglies.add(num * 5);
+            }
+        }
+    }
+}
