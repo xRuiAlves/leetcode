2acf3000482b138199e6f14756a356ad13a14abc
Rui Alves <up201606746@fe.up.pt>
2020-07-09 21:23:14 +0100

    Add july 2020 challenge day 8 solution

---
+++ Monthly Challenges/July 2020 Monthly Challenge/Day8.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Day8 {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new LinkedList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; ++i) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int l = i + 1;
+            int r = nums.length - 1;
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+
+                if (sum == 0) {
+                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    ++l;
+                    --r;
+
+                    while (l < r && nums[l] == nums[l - 1]) {
+                        ++l;
+                    }
+                    while (l < r && nums[r] == nums[r + 1]) {
+                        --r;
+                    }
+                } else if (sum > 0) {
+                    --r;
+                } else {
+                    ++l;
+                }
+            }
+        }
+
+        return res;
+    }
+}
