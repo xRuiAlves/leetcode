6e13a279a4e79e9faa75035752bde5191e620589
Rui Alves <up201606746@fe.up.pt>
2020-07-10 12:14:24 +0100

    Add 430 solution

---
+++ Problems/430. Flatten a Multilevel Doubly Linked List/Solution.java
@@ -0,0 +1,61 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+};
+*/
+
+class Solution {
+    private static final class List {
+        public Node head;
+        public Node tail;
+
+        public List() {}
+
+        public List(Node head, Node tail) {
+            this.head = head;
+            this.tail = tail;
+        }
+    }
+
+    public Node flatten(Node head) {
+        List list = buildList(head);
+        return list.head;
+    }
+
+    private List buildList(Node node) {
+        if (node == null) {
+            return new List();
+        }
+
+        Node curr = node;
+
+        while (curr.next != null || curr.child != null) {
+            if (curr.child != null) {
+                List child_list = buildList(curr.child);
+                curr.child = null;
+                Node after_list = curr.next;
+
+                curr.next = child_list.head;
+                child_list.head.prev = curr;
+
+                child_list.tail.next = after_list;
+                if (after_list != null) {
+                    after_list.prev = child_list.tail;
+                }
+
+                curr = child_list.tail;
+            }
+
+            if (curr.next == null) {
+                break;
+            }
+            curr = curr.next;
+        }
+
+        return new List(node, curr);
+    }
+}
