5c724532ecfc49c049c6a86aa0fcbad6d4003f5d
Rui Alves <up201606746@fe.up.pt>
2020-07-11 09:26:01 +0100

    Add july 2020 challenge day 11 solution

---
+++ Monthly Challenges/July 2020 Monthly Challenge/Day11.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class Day11 {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> sets = new LinkedList<>();
+        int num_iters = (int) Math.pow(2, nums.length);
+        
+        for (int i = 0; i < num_iters; ++i) {
+            List<Integer> set = new LinkedList<>();
+            for (int j = 0; j < nums.length; ++j) {
+                if ((i & (1 << j)) != 0) {
+                    set.add(nums[j]);
+                }
+            }
+            sets.add(set);
+        }
+        
+        return sets;
+    }
+}
