c4f6bb095bde9929ef5e854f2c8292ecfb1c3845
Rui Alves <up201606746@fe.up.pt>
2020-07-12 08:54:53 +0100

    Add july 2020 challenge day 12 solution

---
+++ Monthly Challenges/July 2020 Monthly Challenge/Day12.java
@@ -0,0 +1,15 @@
+public class Day12 {
+    private static final int NUM_BITS = 32;
+
+    public int reverseBits(int n) {
+        int reversed = 0;
+
+        for (int i = 0; i < NUM_BITS; ++i) {
+            int mask = (1 << i);
+            boolean bit_activated = (n & mask) != 0;
+            reversed |= bit_activated ? (1 << (NUM_BITS - i - 1)) : 0;
+        }
+        
+        return reversed;
+    }
+}
