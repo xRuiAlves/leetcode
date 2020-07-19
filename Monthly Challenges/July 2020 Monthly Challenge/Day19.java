9bb89fef054cd8fc970f5765c5bed1b6f6c0c920
Rui Alves <up201606746@fe.up.pt>
2020-07-19 10:07:39 +0100

    Add july 2020 challenge day 19 solution

---
+++ Monthly Challenges/July 2020 Monthly Challenge/Day19.java
@@ -0,0 +1,27 @@
+public class Day19 {
+    public String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int num_iters = Math.max(a.length(), b.length());
+        int carry = 0;
+
+        for (int i = 0; i < num_iters; ++i) {
+            int b1 = getBitValue(a, i);
+            int b2 = getBitValue(b, i);
+
+            int val = b1 + b2 + carry;
+
+            sb.append(val % 2);
+            carry = val < 2 ? 0 : 1;
+        }
+
+        if (carry == 1) {
+            sb.append(1);
+        }
+
+        return sb.reverse().toString();
+    }
+
+    private static int getBitValue(String s, int i) {
+        return i < s.length() ? (s.charAt(s.length() - i - 1) - '0') : 0;
+    }
+}
