998b9d60f88b401c942f0461039d1df46634b75d
Rui Alves <up201606746@fe.up.pt>
2020-07-20 10:53:15 +0100

    Add july 2020 challenge day 20 solution

---
+++ Monthly Challenges/July 2020 Monthly Challenge/Day20.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+public class Day20 {
+    public ListNode removeElements(ListNode head, int val) {
+        while (head != null && head.val == val) {
+            head = head.next;
+        }
+        
+        ListNode curr = head;
+        while (curr != null) {
+            if (curr.next != null && curr.next.val == val) {
+                curr.next = curr.next.next;
+            } else {
+                curr = curr.next;
+            }
+        }
+        
+        return head;
+    }
+}
