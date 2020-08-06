c904f14e2772a918dbb17415ed694a7c60968839
Rui Alves <up201606746@fe.up.pt>
2020-08-06 08:55:54 +0100

    Add 442 Solution

---
+++ Problems/442. Find All Duplicates in an Array/Solution.java
@@ -0,0 +1,33 @@
+import java.util.LinkedList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> dups = new LinkedList<>();
+
+        for (int i = 0; i < nums.length; ++i) {
+            int elem = nums[i];
+
+            if (elem == i + 1) {
+                continue;
+            }
+                
+            if (nums[elem - 1] != elem) {
+                nums[i] = nums[elem - 1];
+                nums[elem - 1] = elem;
+
+                if (nums[i] != i + 1) {
+                    --i;
+                }
+            }
+        }
+        
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] != i + 1) {
+                dups.add(nums[i]);
+            }
+        }
+
+        return dups;
+    }
+}
