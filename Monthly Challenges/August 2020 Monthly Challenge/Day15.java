4782fb29f89ba2c02b25ae2dc72e0de8f695ede4
Rui Alves <up201606746@fe.up.pt>
2020-08-15 11:16:20 +0100

    Add august 2020 challenge day 15 solution

---
+++ Monthly Challenges/August 2020 Monthly Challenge/Day15.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class Day15 {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (int1, int2) -> {
+            if (int1[1] == int2[1]) {
+                return int2[0] - int1[0];
+            }
+            return int1[1] - int2[1];
+        });
+
+        int count = 0;
+        int j = 0;
+
+        for (int i = 1; i < intervals.length; ++i) {
+            if (intervals[j][1] > intervals[i][0]) {
+                ++count;
+            } else {
+                j = i;
+            }
+        }
+
+        return count;
+    }
+}
