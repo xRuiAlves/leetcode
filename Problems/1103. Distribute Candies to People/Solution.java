17a8a7267b8749afb85b3c70a8cd810b44457434
Rui Alves <up201606746@fe.up.pt>
2020-08-17 09:30:54 +0100

    Add 1103 Solution

---
+++ Problems/1103. Distribute Candies to People/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] distributeCandies(int candies, int num_people) {
+        int[] people = new int[num_people];
+        int i = 0;
+
+        while (true) {
+            people[i % num_people] += (i + 1);
+            candies -= (i + 1);
+            
+            if (candies <= 0) {
+                people[i % num_people] -=- candies;
+                break;
+            }
+            ++i;
+        }
+        
+        return people;
+    }
+}
