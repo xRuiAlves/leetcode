b11012588e9aced859fd847b38b9e90765bc043e
Rui Alves <up201606746@fe.up.pt>
2020-08-26 08:33:01 +0100

    Add august 2020 challenge day 26 solution

---
+++ Monthly Challenges/August 2020 Monthly Challenge/Day26.java
@@ -0,0 +1,26 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class Day26 {
+    private static final String FIZZ = "Fizz";
+    private static final String BUZZ = "Buzz";
+    private static final String FIZZBUZZ = "FizzBuzz";
+
+    public List<String> fizzBuzz(int n) {
+        List<String> fizzbuzz = new LinkedList<>();
+
+        for (int i = 1; i <= n; ++i) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                fizzbuzz.add(FIZZBUZZ);
+            } else if (i % 3 == 0) {
+                fizzbuzz.add(FIZZ);
+            } else if (i % 5 == 0) {
+                fizzbuzz.add(BUZZ);
+            } else {
+                fizzbuzz.add(Integer.toString(i));
+            }
+        }
+        
+        return fizzbuzz;
+    }
+}
