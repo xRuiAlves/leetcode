645caab4b452d666e08041e9ec1dfeae3aa02154
Rui Alves <up201606746@fe.up.pt>
2020-09-02 19:59:35 +0100

    Add august 2020 challenge day 31 solution

---
+++ Monthly Challenges/August 2020 Monthly Challenge/Day31.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Day31 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) {
+            return root;
+        } else if (root.val > key) {
+            root.left = deleteNode(root.left, key);
+        } else if (root.val < key) {
+            root.right = deleteNode(root.right, key);
+        } else {
+            if (root.left == null) {
+                return root.right;
+            }
+            if (root.right == null) {
+                return root.left;
+            }
+
+            root.val = getNext(root.right).val;
+            root.right = deleteNode(root.right, root.val);
+        }
+
+        return root;
+    }
+
+    private TreeNode getNext(TreeNode node) {
+        return node.left == null ? node : getNext(node.left);
+    }
+}
