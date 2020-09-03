ea42716a67d4959b04c274511d693a28dc347d1a
Rui Alves <up201606746@fe.up.pt>
2020-09-03 11:18:28 +0100

    Improve 516 Solution

--- Problems/516. Longest Palindromic Subsequence/Solution.java
+++ Problems/516. Longest Palindromic Subsequence/Solution.java
@@ -1,34 +1,19 @@
-import java.util.HashMap;
-
 class Solution {
     public int longestPalindromeSubseq(String s) {
-        HashMap<String, Integer> cache = new HashMap<>();
-        cache.put("", 0);
-        return visit(s, cache);
-    }
+        int n = s.length();
 
-    private int visit(String s, HashMap<String, Integer> cache) {
-        int cached_val = cache.getOrDefault(s, -1);
-        if (cached_val != -1) {
-            return cached_val;
+        int[][] dp = new int[n + 1][n + 1];
+
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (s.charAt(i - 1) == s.charAt(n - j)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
         }
 
-        if (s.length() == 1) {
-            cache.put(s, 1);
-            return 1;
-        }
-
-        int count = 0;
-        if (s.charAt(0) == s.charAt(s.length() - 1)) {
-            count = 2 + visit(s.substring(1, s.length() - 1), cache);
-        } else {
-            count = Math.max(
-                    visit(s.substring(0, s.length() - 1), cache),
-                    visit(s.substring(1, s.length()), cache)
-            );
-        }
-
-        cache.put(s, count);
-        return count;
+        return dp[n][n];
     }
 }
