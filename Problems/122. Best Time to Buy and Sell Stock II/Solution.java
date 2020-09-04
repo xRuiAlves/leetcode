f66bc4c44379fc08a68e5ddc6afeaf3c5e3507a8
Rui Alves <up201606746@fe.up.pt>
2020-09-04 12:04:26 +0100

    Improve 122 Solution

--- Problems/122. Best Time to Buy and Sell Stock II/Solution.java
+++ Problems/122. Best Time to Buy and Sell Stock II/Solution.java
@@ -1,34 +1,11 @@
-import java.util.Arrays;
-
 class Solution {
     public int maxProfit(int[] prices) {
-        int[] cache = new int[prices.length];
-        Arrays.fill(cache, -1);
-        return maxProfit(prices, 0, cache);
-    }
-
-    public int maxProfit(int[] prices, int start, int[] cache) {
-        if (start >= prices.length - 1) {
-            return 0;
+        int profit = 0;
+        
+        for (int i = 1; i < prices.length; ++i) {
+            profit += Math.max(0, prices[i] - prices[i - 1]);
         }
         
-        if (cache[start] != -1) {
-            return cache[start];
-        }
-
-        int best = 0;
-        int min_idx = start;
-
-        for (int i = start + 1; i < prices.length; ++i) {
-            if (prices[i] > prices[min_idx]) {
-                int price_candidate = prices[i] - prices[min_idx] + maxProfit(prices, i + 1, cache);
-                best = Math.max(best, price_candidate);
-            } else if (prices[i] < prices[min_idx]) {
-                min_idx = i;
-            }
-        }
-
-        cache[start] = best;
-        return best;
+        return profit;
     }
 }
