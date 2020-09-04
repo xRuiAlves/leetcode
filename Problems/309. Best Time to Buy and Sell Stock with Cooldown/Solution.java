56b8035473b5ea4657114f3cc70a0fb5a1a28815
Rui Alves <up201606746@fe.up.pt>
2020-09-04 12:46:08 +0100

    Improve 309 Solution

--- Problems/309. Best Time to Buy and Sell Stock with Cooldown/Solution.java
+++ Problems/309. Best Time to Buy and Sell Stock with Cooldown/Solution.java
@@ -1,24 +1,21 @@
 class Solution {
-    public static int maxProfit(int[] prices) {
-        if (prices.length == 0) {
+    public int maxProfit(int[] prices) {
+        if (prices.length < 2) {
             return 0;
         }
-        
+
         int[] dp = new int[prices.length];
-        int increment = Integer.MIN_VALUE;
-        
-        for (int i = 0; i < prices.length; ++i) {  
-        	if (i > 0) {
-        		dp[i] = Math.max(0, Math.max(dp[i-1], prices[i] + increment));
+        int loss = Integer.MAX_VALUE;
+
+        for (int i = 0; i < prices.length; ++i) {
+            if (i > 0) {
+                dp[i] = Math.max(dp[i - 1], prices[i] - loss);
             }
 
-        	int last_val = (i > 2) ? dp[i -2] : 0;
-
-        	if (last_val > prices[i] + increment) {
-                increment = last_val - prices[i];
-            }
+            int last_val = i > 2 ? dp[i - 2] : 0;
+            loss = Math.min(loss, prices[i] - last_val);
         }
-        
-        return dp[dp.length - 1];       
+
+        return dp[dp.length - 1];
     }
 }
