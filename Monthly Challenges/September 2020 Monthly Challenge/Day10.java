3b62fe1148941d5ee8448e53b8885f5c05474ae3
Rui Alves <up201606746@fe.up.pt>
2020-09-10 09:49:24 +0100

    Add september 2020 challenge day 10 solution

---
+++ Monthly Challenges/September 2020 Monthly Challenge/Day10.java
@@ -0,0 +1,31 @@
+public class Day10 {
+    private static final int ALPHABET_START = '0';
+    private static final int ALPHABET_END = '9';
+    private static final int ALPHABET_SIZE = ALPHABET_END - ALPHABET_START + 1;
+
+    public String getHint(String secret, String guess) {
+        int bulls = 0;
+        int[] char_counts = new int[ALPHABET_SIZE];
+
+        for (int i = 0; i < secret.length(); ++i) {
+            if (secret.charAt(i) == guess.charAt(i)) {
+                ++bulls;
+            } else {
+                ++char_counts[secret.charAt(i) - ALPHABET_START];
+            }
+        }
+
+        int cows = 0;
+        for (int i = 0; i < guess.length(); ++i) {
+            if (secret.charAt(i) != guess.charAt(i)){
+                int char_idx = guess.charAt(i) - ALPHABET_START;
+                if (char_counts[char_idx] > 0) {
+                    ++cows;
+                    --char_counts[char_idx];
+                }
+            }
+        }
+
+        return String.format("%dA%dB", bulls, cows);
+    }
+}
