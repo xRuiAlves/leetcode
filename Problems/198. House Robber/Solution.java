1292c3a9f1a9c0f0feba486eef0e94887e7658a9
Rui Alves <up201606746@fe.up.pt>
2020-09-14 09:08:52 +0100

    Add 198 Solution

---
+++ Problems/198. House Robber/Solution.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+class Solution {
+    private int[] robberies;
+    
+    public int rob(int[] nums) {
+        robberies = new int[nums.length];
+        Arrays.fill(robberies, -1);
+        return rob(nums, 0);
+    }
+
+    private int rob(int[] nums, int day) {
+        if (day >= nums.length) {
+            return 0;
+        }
+        
+        if (robberies[day] != -1) {
+            return robberies[day];
+        }
+
+        robberies[day] = Math.max(nums[day] + rob(nums, day + 2), rob(nums, day + 1));
+        return robberies[day];
+    }
+}
