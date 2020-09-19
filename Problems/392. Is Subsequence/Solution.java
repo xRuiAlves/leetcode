8693d735c408c2c8cb5cbf6709cb1cda0d4513c4
Rui Alves <up201606746@fe.up.pt>
2020-09-19 11:13:33 +0100

    Improve 392 Solution

--- Problems/392. Is Subsequence/Solution.java
+++ Problems/392. Is Subsequence/Solution.java
@@ -1,70 +1,17 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        HashMap<Character, ArrayList<Integer>> occurences = new HashMap<>();
+        int p1 = 0;
+        int p2 = 0;
 
-        for (int i = 0; i < t.length(); ++i) {
-            char c = t.charAt(i);
-            ArrayList<Integer> char_occurences = occurences.getOrDefault(c, null);
-
-            if (char_occurences == null) {
-                char_occurences = new ArrayList<>();
-                occurences.put(c, char_occurences);
-            }
-
-            char_occurences.add(i);
-        }
-
-        int curr = -1;
-        for (char c : s.toCharArray()) {
-            ArrayList<Integer> char_occurences = occurences.getOrDefault(c, null);
-            if (char_occurences == null) {
-                return false;
-            }
-
-            int idx = getNext(char_occurences, curr);
-            if (idx == -1) {
-                return false;
-            }
-            curr = char_occurences.get(idx);
-        }
-
-        return true;
-    }
-
-    private int getNext(ArrayList<Integer> arr, int num) {
-        int l = 0;
-        int r = arr.size() - 1;
-
-        while (l + 1 < r) {
-            int m = (l + r) / 2;
-            int elem = arr.get(m);
-
-            if (elem == num) {
-                if (m == arr.size() - 1) {
-                    return -1;
-                } else {
-                    return m + 1;
-                }
-            }
-            if (elem > num) {
-                r = m;
-            } else if (elem < num) {
-                l = m;
+        while (p1 < s.length() && p2 < t.length()) {
+            if (s.charAt(p1) == t.charAt(p2)) {
+                ++p1;
+                ++p2;
+            } else {
+                ++p2;
             }
         }
 
-        if (arr.get(l) > num) {
-            return l;
-        }
-        if (arr.get(r) > num) {
-            return r;
-        }
-        if (r == arr.size() - 1) {
-            return -1;
-        }
-        return r + 1;
+        return p1 == s.length();
     }
 }
