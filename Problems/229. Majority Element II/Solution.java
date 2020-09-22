1012cd1b8dbad76c21779aa5ac30d05210ab12e7
Rui Alves <up201606746@fe.up.pt>
2020-09-22 09:12:05 +0100

    Add 229 Solution

--- Problems/229. Majority Element II/Solution.java
+++ Problems/229. Majority Element II/Solution.java
@@ -1,17 +1,50 @@
-import java.util.*;
+import java.util.LinkedList;
+import java.util.List;
 
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        Map<Integer, Integer> counts = new HashMap<>();
-        List<Integer> res = new LinkedList<>();
-        int n = nums.length / 3;
+        Integer candidate1 = null;
+        Integer candidate2 = null;
+        int c1 = 0;
+        int c2 = 0;
+
         for (int num : nums) {
-            int count = counts.getOrDefault(num, 0) + 1;
-            counts.put(num, count);
-            if (count == n + 1) {
-                res.add(num);
+            if (candidate1 != null && num == candidate1) {
+                ++c1;
+            } else if (candidate2 != null && num == candidate2) {
+                ++c2;
+            } else if (c1 == 0) {
+                candidate1 = num;
+                ++c1;
+            } else if (c2 == 0) {
+                candidate2 = num;
+                ++c2;
+            } else {
+                --c1;
+                --c2;
             }
         }
-        return res;
+
+        List<Integer> majorities = new LinkedList<>();
+        int target_size = nums.length / 3;
+        c1 = 0;
+        c2 = 0;
+        
+        for (int num : nums) {
+            if (num == candidate1) {
+                ++c1;
+            } else if (num == candidate2) {
+                ++c2;
+            }
+        }
+        
+        if (c1 > target_size) {
+            majorities.add(candidate1);
+        }
+        if (c2 > target_size) {
+            majorities.add(candidate2);
+        }
+        
+        return majorities;
     }
 }
