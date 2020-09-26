511b364f5e4c12980b792e7b159b9ceba0edf6c1
Rui Alves <up201606746@fe.up.pt>
2020-09-26 19:46:43 +0100

    Add 495 Solution

---
+++ Problems/495. Teemo Attacking/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findPoisonedDuration(int[] timeSeries, int duration) {
+        int time = 0;
+        if (timeSeries.length == 0) {
+            return time;
+        }
+        
+        time = duration * timeSeries.length;
+        for (int i = 1; i < timeSeries.length; ++i) {
+            time += Math.min(0, timeSeries[i] - timeSeries[i - 1] - duration);
+        }
+        
+        return time;
+    }
+}
