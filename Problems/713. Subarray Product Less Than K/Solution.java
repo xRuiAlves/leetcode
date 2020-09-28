1672a3b32866fe1ec67ebb166ac168abb26542d4
Rui Alves <up201606746@fe.up.pt>
2020-09-28 09:25:07 +0100

    Add 713 Solution

---
+++ Problems/713. Subarray Product Less Than K/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if (k <= 1) {
+            return 0;
+        }
+
+        int count = 0;
+        int lb = 0;
+        int prod = 1;
+
+        for (int rb = 0; rb < nums.length; ++rb) {
+            prod *= nums[rb];
+
+            while (prod >= k) {
+                prod /= nums[lb++];
+            }
+
+            count += rb - lb + 1;
+        }
+
+        return count;
+    }
+}
