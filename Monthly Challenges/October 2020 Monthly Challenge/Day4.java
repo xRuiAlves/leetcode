4aada6b258d3060c74c62881c0d8381120ebf364
Rui Alves <up201606746@fe.up.pt>
2020-10-05 09:08:16 +0100

    Add october 2020 challenge day 4 solution

---
+++ Monthly Challenges/October 2020 Monthly Challenge/Day4.java
@@ -0,0 +1,23 @@
+public class Day4 {
+    public int removeCoveredIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (i1, i2) -> {
+           if (i1[0] == i2[0]) {
+               return i2[1] - i1[1];
+           }
+            return i1[0] - i2[0];
+        });
+        
+        int total = intervals.length;
+        
+        for (int i = 0; i < intervals.length; ++i) {
+            int[] curr = intervals[i];
+            
+            for (int j = i + 1; j < intervals.length && intervals[j][1] <= curr[1]; ++j) {
+                ++i;
+                --total;
+            }
+        }
+        
+        return total;
+    }
+}
