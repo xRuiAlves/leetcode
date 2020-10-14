4e5b50ff60428be4d78c66e7a4ab137c31d5db37
Rui Alves <up201606746@fe.up.pt>
2020-10-14 09:39:21 +0100

    Add 213 Solution

---
+++ Problems/213. House Robber II/Solution.java
@@ -0,0 +1,32 @@
+class Solution {
+    Integer[] cache;
+
+    public int rob(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        
+        cache = new Integer[nums.length];
+        int profit = rob(nums, 0, nums.length - 2);
+
+        cache = new  Integer[nums.length];
+        profit = Math.max(profit, rob(nums, 1, nums.length - 1));
+
+        return profit;
+    }
+
+    private int rob(int[] nums, int curr_day, int last_day) {
+        if (curr_day > last_day) {
+            return 0;
+        }
+
+        if (cache[curr_day] == null) {
+            cache[curr_day] = Math.max(
+                nums[curr_day] + rob(nums, curr_day + 2, last_day),
+                rob(nums, curr_day + 1, last_day)
+            );
+        }
+
+        return cache[curr_day];
+    }
+}
