ec718a530dd7f38358c805979ed73818cedaa63f
Rui Alves <up201606746@fe.up.pt>
2020-10-17 12:37:01 +0100

    Add october 2020 challenge day 17 solution (scala)

---
+++ Monthly Challenges/October 2020 Monthly Challenge/Day17.scala
@@ -0,0 +1,16 @@
+import scala.collection.mutable
+
+object Day17 {
+    val SEQUENCE_SIZE : Int = 10
+
+    def findRepeatedDnaSequences(s: String): List[String] = {
+        val sequence_counts = new mutable.HashMap[String, Int]()
+
+        for (i <- 0 to (s.length - SEQUENCE_SIZE)) {
+            val sequence : String = s.substring(i, i + SEQUENCE_SIZE)
+            sequence_counts(sequence) = sequence_counts.getOrElse(sequence, 0) + 1
+        }
+
+        sequence_counts.filter(_._2 > 1).keys.toList
+    }
+}
