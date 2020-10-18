eb64b1b230a4be24eee386523d92232e996a47e7
Rui Alves <up201606746@fe.up.pt>
2020-10-18 09:28:57 +0100

    Add october 2020 challenge day 18 solution (scala)

---
+++ Monthly Challenges/October 2020 Monthly Challenge/Day18.scala
@@ -0,0 +1,21 @@
+object Solution {
+    def maxProfit(k: Int, prices: Array[Int]): Int = {
+        if (k == 0 || prices.length < 2) return 0
+        val max_transactions = Math.min(k, prices.length / 2);
+
+        val purchases : Array[Int] = Array.fill(max_transactions)(-prices(0))
+        val sales : Array[Int] = new Array[Int](max_transactions)
+        
+        for (i <- 1 until prices.length) {
+            purchases(0) = math.max(purchases(0), -prices(i))
+            sales(0) = math.max(sales(0), purchases(0) + prices(i))
+            
+            for (j <- 1 until max_transactions) {
+                purchases(j) = math.max(purchases(j), sales(j - 1) - prices(i))
+                sales(j) = math.max(sales(j), purchases(j) + prices(i))
+            }
+        }
+        
+        sales(max_transactions - 1)
+    }
+}
