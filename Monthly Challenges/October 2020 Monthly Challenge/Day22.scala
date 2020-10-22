3505447462ba983fd0ad053ae5d3ea896db790d2
Rui Alves <up201606746@fe.up.pt>
2020-10-22 09:21:49 +0100

    Add october 2020 challenge day 22 solution (scala)

---
+++ Monthly Challenges/October 2020 Monthly Challenge/Day22.scala
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
+ *   var value: Int = _value
+ *   var left: TreeNode = _left
+ *   var right: TreeNode = _right
+ * }
+ */
+
+object Day22 {
+    def minDepth(root: TreeNode): Int = if (root == null) 0 else minDepth(root, 1)
+
+    def minDepth(node: TreeNode, depth: Int): Int = (node.left, node.right) match {
+        case (null, null) => depth
+        case (left, null) => minDepth(left, depth + 1)
+        case (null, right) => minDepth(right, depth + 1)
+        case (left, right) => math.min(
+            minDepth(left, 1 + depth),
+            minDepth(right, 1 + depth)
+        )
+    }
+}
