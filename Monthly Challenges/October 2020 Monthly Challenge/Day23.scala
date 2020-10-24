0ff135b5ce550b177c17322c73fc39f80b48aa48
Rui Alves <up201606746@fe.up.pt>
2020-10-24 10:54:14 +0100

    Add october 2020 challenge day 23 solution (scala)

---
+++ Monthly Challenges/October 2020 Monthly Challenge/Day23.scala
@@ -0,0 +1,20 @@
+object Day23 {
+    def find132pattern(nums: Array[Int]): Boolean = {
+        for (i <- nums.indices) {
+            if (hasGrowingPair(nums, i)) return true
+        }
+        false
+    }
+
+    def hasGrowingPair(nums: Array[Int], lb: Int): Boolean = {
+        var min = Int.MaxValue
+        
+        for (i <- (nums.length - 1) until lb by - 1) {
+            if (nums(i) > nums(lb)) {
+                min = math.min(min, nums(i))
+                if (nums(i) > min) return true
+            }
+        }
+        false
+    }
+}
