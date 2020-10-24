fa71e9a55b20a2be2203f4e9c8c2c19db0d760f0
Rui Alves <up201606746@fe.up.pt>
2020-10-24 10:57:23 +0100

    Add 948 Solution (scala)

---
+++ Problems/948. Bag of Tokens/Solution.scala
@@ -0,0 +1,26 @@
+object Solution {
+    def bagOfTokensScore(tokens: Array[Int], P: Int): Int = {
+        val sorted_tokens = tokens.sortWith(_<_);
+        var i0 = 0
+        var i1 = sorted_tokens.length - 1
+        var points = P
+        var curr_score = 0
+        var best_score = curr_score
+
+        while (i0 <= i1 && (points >= sorted_tokens(i0) || curr_score > 0)) {
+            if (points >= sorted_tokens(i0)) {
+                points -= sorted_tokens(i0)
+                i0 += 1
+                curr_score += 1
+                best_score = math.max(best_score, curr_score)
+            } else {
+                if (curr_score > 0) {
+                    points += sorted_tokens(i1)
+                    i1 -= 1
+                    curr_score -= 1
+                }
+            }
+        }
+        best_score
+    }
+}
