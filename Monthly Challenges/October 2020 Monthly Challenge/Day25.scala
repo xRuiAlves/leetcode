172792cccd8316346705858de61c27b1b4ded84b
Rui Alves <up201606746@fe.up.pt>
2020-10-25 09:59:11 +0000

    Add october 2020 challenge day 25 solution (scala)

---
+++ Monthly Challenges/October 2020 Monthly Challenge/Day25.scala
@@ -0,0 +1,30 @@
+import scala.collection.mutable.ArrayBuffer
+
+object Day25 {
+    def winnerSquareGame(n: Int): Boolean = {
+        val dp = new Array[Boolean](n + 1)
+        val exponents = buildExponents(n)
+
+        for (i <- dp.indices) {
+            var j = 0
+            while (j < exponents.length && i - exponents(j) >= 0 && !dp(i)) {
+                dp(i) = dp(i) || !dp(i - exponents(j))
+                j += 1
+            }
+        }
+
+        dp(n)
+    }
+
+    def buildExponents(n: Int): Array[Int] = {
+        val exponents = new ArrayBuffer[Int]()
+
+        var i = 1
+        while (i*i <= n) {
+            exponents += (i * i)
+            i += 1
+        }
+
+        exponents.toArray
+    }
+}
