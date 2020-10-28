9aa5d4cd6ba23332902e47426e3dfa635c608e2a
Rui Alves <up201606746@fe.up.pt>
2020-10-28 17:21:04 +0000

    Add 142 Solution (scala)

---
+++ Problems/142. Linked List Cycle II/Solution.scala
@@ -0,0 +1,26 @@
+import scala.collection.mutable
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode(var _x: Int = 0) {
+ *   var next: ListNode = null
+ *   var x: Int = _x
+ * }
+ */
+
+object Solution {
+    def detectCycle(head: ListNode): ListNode = {
+        val visited = new mutable.HashSet[ListNode]()
+        var curr = head
+
+        while (curr != null) {
+            if (visited.contains(curr)) return curr
+            else {
+                visited.add(curr)
+                curr = curr.next
+            }
+        }
+
+        null
+    }
+}
