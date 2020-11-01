43861d1feca5ce021d25597afa5e97a504001d6c
Rui Alves <up201606746@fe.up.pt>
2020-11-01 14:50:05 +0000

    Add 673 Solution (scala)

---
+++ Problems/673. Number of Longest Increasing Subsequence/Solution.scala
@@ -0,0 +1,26 @@
+object Solution {
+    def findNumberOfLIS(nums: Array[Int]): Int = {
+        if (nums.length <= 1) return nums.length
+
+        val sizes = new Array[Int](nums.length)
+        val counts = Array.fill(nums.length)(1)
+
+        for (j <- counts.indices; i <- 0 until j) {
+            if (nums(i) < nums(j)) {
+                if (sizes(i) >= sizes(j)) {
+                    sizes(j) = sizes(i) + 1;
+                    counts(j) = counts(i);
+                } else if (sizes(i) + 1 == sizes(j)) {
+                    counts(j) += counts(i);
+                }
+            }
+        }
+
+        val max = sizes.max
+        var res = 0
+        for (i <- sizes.indices) {
+            if (sizes(i) == max) res += counts(i)
+        }
+        res
+    }
+}
