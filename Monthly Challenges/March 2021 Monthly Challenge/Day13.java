15379d340ceb60b28db69d541da514e4120d64db
Rui Alves <up201606746@fe.up.pt>
2021-03-13 16:16:34 +0000

    Add march 2021 challenge day 13 solution

---
+++ Monthly Challenges/March 2021 Monthly Challenge/Day13.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Day13 {
+    private static final int MODULO = (int) (1e9 + 7);
+
+    public int numFactoredBinaryTrees(int[] arr) {
+        Arrays.sort(arr);
+        Map<Integer, Long> tree_counts = new HashMap<>();
+
+        for (int num : arr) {
+            tree_counts.put(num, null);
+        }
+
+        for (int i = 0; i < arr.length; ++i) {
+            long count = 1;
+            for (int j = i - 1; j >= 0; --j) {
+                int mod = arr[i] % arr[j];
+                int div = arr[i] / arr[j];
+                if (mod == 0 && tree_counts.containsKey(div)) {
+                    count += tree_counts.get(arr[j]) * tree_counts.get(div);
+                }
+            }
+            tree_counts.put(arr[i], count);
+        }
+
+        return (int) (tree_counts.values().stream().mapToLong(i -> i).sum() % MODULO);
+    }
+}
