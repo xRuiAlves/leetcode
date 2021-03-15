de20e077a3c94f2f044f1e364dc71a2bb797b949
Rui Alves <up201606746@fe.up.pt>
2021-03-15 11:03:08 +0000

    Add 535 Solution

---
+++ Problems/535. Encode and Decode TinyURL/Solution.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    private static final Map<String, String> encodings = new HashMap<>();
+    private static final String TINYURL_PREFIX = "http://tinyurl.com/";
+    private static int ENCODE_COUNT = 0;
+
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+        String shortUrl = TINYURL_PREFIX + ENCODE_COUNT++;
+        encodings.put(shortUrl, longUrl);
+        return shortUrl;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        return encodings.get(shortUrl);
+    }
+}
