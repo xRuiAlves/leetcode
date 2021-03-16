9ca6879649ba9fab2ea25fce8c331f26c01049ad
Rui Alves <up201606746@fe.up.pt>
2021-03-16 11:43:00 +0000

    Add march 2021 challenge day 16 solution

---
+++ Monthly Challenges/March 2021 Monthly Challenge/Day16.java
@@ -0,0 +1,40 @@
+public class Day16 {
+    private Integer[] buy_cache;
+    private Integer[] sell_cache;
+
+    public int maxProfit(int[] prices, int fee) {
+        buy_cache = new Integer[prices.length];
+        sell_cache = new Integer[prices.length];
+        return buy(prices, fee, 0);
+    }
+
+    private int buy(int[] prices, int fee, int day) {
+        if (day >= prices.length) {
+            return 0;
+        }
+
+        if (buy_cache[day] == null) {
+            buy_cache[day] = Math.max(
+                buy(prices, fee, day + 1),
+                sell(prices, fee, day + 1) - prices[day] - fee
+            );
+        }
+
+        return buy_cache[day];
+    }
+
+    private int sell(int[] prices, int fee, int day) {
+        if (day >= prices.length) {
+            return 0;
+        }
+
+        if (sell_cache[day] == null) {
+            sell_cache[day] = Math.max(
+                sell(prices, fee, day + 1),
+                buy(prices, fee, day + 1) + prices[day]
+            );
+        }
+
+        return sell_cache[day];
+    }
+}
