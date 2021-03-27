efb8dea7c9a285ca8795ea287da8710674ec61b3
Rui Alves <up201606746@fe.up.pt>
2021-03-27 08:30:45 +0000

    Add march 2021 challenge day 28 solution

---
+++ Monthly Challenges/March 2021 Monthly Challenge/Day28.java
@@ -0,0 +1,28 @@
+public class Day28 {
+    public int countSubstrings(String s) {
+        int res = 0;
+        
+        for (int i = 0; i < s.length(); ++i) {
+            ++res;
+            int l = i, r = i;
+            while (r < s.length() - 1 && s.charAt(r) == s.charAt(r + 1)) {
+                ++r;
+                res += (r - i + 1);
+            }
+            int next_index = r;
+
+            while(l > 0 && r < s.length() - 1) {
+                --l;
+                ++r;
+                if (s.charAt(l) != s.charAt(r)) {
+                    break;
+                }
+                ++res;
+            }
+
+            i = next_index;
+        }
+
+        return res;
+    }
+}
