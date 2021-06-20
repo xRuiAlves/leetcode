b90ad2addd12464559544dcf514699c23e0ca4f6
xRuiAlves <ruialves.esrt.98@gmail.com>
2021-06-20 10:30:14 +0100

    Add june 2021 challenge day 2 solution

---
+++ Monthly Challenges/June 2021 Monthly Challenge/Day2.java
@@ -0,0 +1,43 @@
+public class Day2 {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        if (s1.length() + s2.length() != s3.length()) {
+            return false;
+        }
+        boolean[][] cache = new boolean[s1.length()][s2.length()];
+
+        return isInterleave(s1, s2, s3, 0, 0, cache);
+    }
+
+    private boolean isInterleave(String s1, String s2, String s3, int i, int j, boolean[][] cache) {
+        if (i + j == s3.length()) {
+            return true;
+        }
+
+        if (i == s1.length()) {
+            return s2.substring(j).equals(s3.substring(i + j));
+        }
+
+        if (j == s2.length()) {
+            return s1.substring(i).equals(s3.substring(i + j));
+        }
+
+        if (cache[i][j]) {
+            return false;
+        }
+
+        if (i < s1.length() && s1.charAt(i) == s3.charAt(i + j)) {
+            if (isInterleave(s1, s2, s3, i + 1, j, cache)) {
+                return true;
+            }
+        }
+
+        if (j < s2.length() && s2.charAt(j) == s3.charAt(i + j)) {
+            if (isInterleave(s1, s2, s3, i, j + 1, cache)) {
+                return true;
+            }
+        }
+
+        cache[i][j] = true;
+        return false;
+    }
+}
