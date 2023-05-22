ee6380b4a941460f66b22e7396dbe53532001bb7
xRuiAlves <ruialves.esrt.98@gmail.com>
2023-05-22 09:29:31 +0100

    Improve 347 Solution (scala)

--- Problems/347. Top K Frequent Elements/Solution.scala
+++ Problems/347. Top K Frequent Elements/Solution.scala
@@ -1,13 +1,10 @@
 import scala.collection.mutable
 
 object Solution {
-  def topKFrequent(nums: Array[Int], k: Int): Array[Int] = {
-    val frequencies = mutable.HashMap[Int, Int]().withDefaultValue(0)
-    nums.foreach(num => frequencies.put(num, frequencies(num) + 1))
-    frequencies
-      .toArray
-      .sortBy(_._2)
-      .takeRight(k)
-      .map(_._1)
-  }
+  def topKFrequent(nums: Array[Int], k: Int): Array[Int] = nums
+    .groupMapReduce(identity)(_ => 1)(_ + _)
+    .toArray
+    .sortBy(_._2)
+    .takeRight(k)
+    .map(_._1)
 }
