09f5ae133f008192a4b3cefc657f82ebab53b71e
xRuiAlves <ruialves.esrt.98@gmail.com>
2023-06-12 10:51:50 +0100

    Improve 228 Solution (scala)

--- Problems/228. Summary Ranges/Solution.scala
+++ Problems/228. Summary Ranges/Solution.scala
@@ -1,16 +1,11 @@
 object Solution {
-  type Range = (Int, Int)
-
-  private[this] final def formatRange(range: Range): String = range match {
-    case (l, b) if l != b => s"$l->$b"
-    case (l, _) => l.toString
-  }
-
   def summaryRanges(nums: Array[Int]): List[String] = nums.
-    foldLeft(List.empty[Range]) {
-      case ((l, r) :: tail, curr) if r == curr - 1 => (l, curr) :: tail
-      case (list, curr) => (curr, curr) :: list
+    foldRight(List.empty[(Int, Int)]) {
+      case (curr, (l, r) :: tail) if curr + 1 == l => (curr, r) :: tail
+      case (curr, list) => (curr, curr) :: list
     }
-    .map(formatRange)
-    .reverse
+    .map {
+      case (l, b) if l != b => s"$l->$b"
+      case (l, _) => l.toString
+    }
 }
