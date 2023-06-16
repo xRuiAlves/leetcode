46b72e6813b55e0e494f19cda5ca5269b2247b59
xRuiAlves <ruialves.esrt.98@gmail.com>
2023-06-16 08:08:57 +0100

    Simplify 530 Solution

--- Problems/530. Minimum Absolute Difference in BST/Solution.scala
+++ Problems/530. Minimum Absolute Difference in BST/Solution.scala
@@ -1,23 +1,10 @@
-import scala.collection.mutable.ArrayBuffer
-
 object Solution {
-  def getMinimumDifference(root: TreeNode): Int = inorder(root)
-    .sliding(2)
-    .map {
-      case ArrayBuffer(n1, n2) => n2 - n1
-    }
-    .min
-
-  def inorder(node: TreeNode): ArrayBuffer[Int] = {
-    val ordering = ArrayBuffer[Int]()
-
-    def visit(node: TreeNode): Unit = if (node != null) {
-      visit(node.left)
-      ordering.addOne(node.value)
-      visit(node.right)
-    }
-
-    visit(node)
-    ordering
+  def getMinimumDifference(node: TreeNode, lb: Long = Int.MinValue, ub: Long = Int.MaxValue): Int = node match {
+    case null => Int.MaxValue
+    case _ =>
+      val diff = math.min(math.abs(ub - node.value), math.abs(node.value - lb))
+      val leftDiff = getMinimumDifference(node.left, lb, node.value)
+      val rightDiff = getMinimumDifference(node.right, node.value, ub)
+      math.min(diff, math.min(leftDiff, rightDiff)).toInt
   }
 }
